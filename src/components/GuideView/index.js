eaf475a7deedf0e5aa621bf09978d50bc02bdb7a
arcthur <arthurtemptation@gmail.com>
2016-01-14 15:28:47 +0800

    init gh-pages

--- src/components/GuideView/index.js
+++
@@ -1,9 +0,0 @@
-import Installation from './Installation';
-import GettingStarted from './GettingStarted';
-import Customize from './Customize';
-
-export {
-  Installation,
-  GettingStarted,
-  Customize,
-};
